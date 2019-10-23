d3d7d0aef83700d2f7d03c56849d86e9bb63cf00
Vbon <5914018@qq.com>
2019-10-23 11:34:42 +0800

    update
    update

--- jd_lucky_money_activity.js
+++ jd_lucky_money_activity.js
@@ -3,14 +3,14 @@
 
 // @name              京东双十一红包插件2019
 
-// @description       2019双十一自动做任务：逛商品，逛店铺，好玩互动，视频直播，精彩会场，一劳永逸。使用插件前请提前登录京东：http://www.jd.com，登录完了之后打开红包活动地址：https://happy.m.jd.com/babelDiy/GZWVJFLMXBQVEBDQZWMY/XJf8bH6oXDWSgS91daDJzXh9bU7/index.html，脚本自动执行。做完任务后关闭页面就可以了。本插件源于互联网分享，与本人无关，本人并不为脚步所产生的结果负责，仅供参考学习，请勿转载使用分发，请您自行辨别本脚步可能导致的风险。本脚步开源至：https://github.com/vbonluk/jd_lucky_money_activity，欢迎star。
+// @description       2019双十一自动做任务：逛商品，逛店铺，好玩互动，视频直播，精彩会场，一劳永逸。
 
 // @homepageURL       https://github.com/vbonluk/JDLuckyMoneyActivity
 // @supportURL        https://github.com/vbonluk/JDLuckyMoneyActivity/issues/
 // @updateURL         https://github.com/vbonluk/JDLuckyMoneyActivity/jd_lucky_money_activity.js
 
 // @author            vbonluk
-// @version           0.0.6
+// @version           0.0.7
 // @license           MIT
 
 // @compatible        chrome Chrome_46.0.2490.86 + TamperMonkey + 脚本_1.3 测试通过
